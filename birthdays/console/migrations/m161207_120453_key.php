9246452bfd95d0cd8963111eb9e0781d8d1bdab2
maximtraulko <max.traulko@bk.ru>
2016-12-07 20:16:46 +0700

    Приложение "Birthdays"

--- birthdays/console/migrations/m161207_120453_key.php
+++ birthdays/console/migrations/m161207_120453_key.php
@@ -7,9 +7,9 @@ class m161207_120453_key extends Migration
     public function up()
     {
 
-	$this->execute (ALTER TABLE `depatment` CHANGE `department_id` `department_id` INT(100) NOT NULL AUTO_INCREMENT;
+	$this->execute ("ALTER TABLE `depatment` CHANGE `department_id` `department_id` INT(100) NOT NULL AUTO_INCREMENT;
 	ALTER TABLE `employee` CHANGE `id` `id` INT(100) NOT NULL AUTO_INCREMENT;
-	ALTER TABLE `employee` ADD FOREIGN KEY (`department_id`) REFERENCES `birthdays`.`depatment`(`department_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;);
+	ALTER TABLE `employee` ADD FOREIGN KEY (`department_id`) REFERENCES `birthdays`.`depatment`(`department_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;");
 
 	
 	
